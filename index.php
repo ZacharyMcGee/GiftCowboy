8fe46c986e8561dbcf1b0f6941eacfa0ecb862d2
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-05 21:30:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,10 +16,25 @@
 
 <body>
 
+<div class="account-bar">
+  <div class="login-signup">
+    <p>Save your favorite gifts by <a href="">logging in</a> or <a href="">signing up</a></p> 
+  </div>
+</div>
+
 <div class="header">
   <div class="logo">
     <img src="images/logo.png">
   </div>
+
+  <div class="menu">
+  <ul class="menu-ul">
+    <li><a class="active" href="#home">Home</a></li>
+    <li><a href="#news">News</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+  </ul>
+</div>
 </div>
 
 <div id="container" class="container">
