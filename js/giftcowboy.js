9b61c3288ba619f2efa75c4569853ab770a2b3d5
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-08 07:11:00 -0500

    Update giftcowboy.js

--- js/giftcowboy.js
+++ js/giftcowboy.js
@@ -1,11 +1,46 @@
 var loadIndex = 0;
 var generated = false;
+var curImageURL = "";
 
 $(document).ready(function(){
     // Load more data
     $('#generate').click(function(){
       hideFilter();
     });
+
+     $( '#create-product' ).on( 'click', function (evt) {
+       evt.stopImmediatePropagation();
+       console.log("HEY");
+       createProduct();
+     });
+
+     $( '#uploadImage' ).on( 'change' , function() {
+       var formData = new FormData();
+       var input = $('input[type=file]')[0].files[0];
+       formData.append('file', input);
+
+       curImageURL = "images/products/" + input.name;
+
+       var reader = new FileReader();
+       reader.onload = function(e) {
+         $('#preview').attr('src', e.target.result);
+       }
+       reader.readAsDataURL(input);
+
+       //document.getElementById('preview').appendChild(fr);
+
+       $.ajax({
+           url : "functions/upload-image.php",
+           type: "POST",
+           data:  formData,
+           contentType: false,
+           cache: false,
+           processData:false,
+           success: function(response){
+               alert(response);
+           }
+       });
+     });
 
     $(window).scroll(function () {
       if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
@@ -76,6 +111,36 @@ function loadProducts() {
         });
 }
 
+
+function createProduct() {
+  var gender = $('input[name=gender]:checked', '#gender').attr('id');
+  var age = $('input[name=age]:checked', '#age').attr('id');
+  var title = $('#title').val();
+  var url = $('#url').val();
+  var description = $('#description').val();
+  var price = $('#price').val();
+
+  var interests = [];
+
+  $(".active-interest").each(function() {
+    interests.push($(this).attr('id'));
+  });
+      $.ajax({
+          url: 'functions/create-product.php',
+          type: 'post',
+          data: {imageurl:curImageURL, tag:JSON.stringify(interests), gender:gender, age:age, title:title, url:url, description:description, price:price},
+          beforeSend:function(){
+              console.log("INSIDE");
+              $("#create-product").text("Loading...");
+            },
+            success: function(response){
+
+                  $('#new-gift').append(response);
+                  $("#create-product").text("Generate");
+                }
+        });
+}
+
 function hideFilter() {
   $( "#gift-filter" ).fadeOut( "slow", function() {
     var giftResultDiv = "<div class='products' id='gift-results'><div class='content-title'><p><img src='images/gift.png'> Gift Results</p></div></div>";
@@ -87,5 +152,4 @@ function hideFilter() {
 function showLogin() {
   var loginDiv = "<div class='login' id='login'><div class='content-title'><p><img src='images/user.png'> Login</p></div></div>";
   $("#container").html = loginDiv;
-  loadProducts();
 }
