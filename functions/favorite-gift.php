8764aa98f59e75daa205d518ea9e3e91eef05c84
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-13 06:40:58 -0500

    Update favorite-gift.php

--- functions/favorite-gift.php
+++ functions/favorite-gift.php
@@ -15,11 +15,29 @@
    $sql = "SELECT id, accountid, giftid FROM favorited WHERE accountid = " . $_SESSION['id'] . " AND giftid = " . $giftid;
 
    $result = $conn->query($sql);
-   if (!$result) {
-     echo '1';
+
+   if ($result->num_rows == 0) {
+     $sql_add_like = "INSERT INTO favorited (accountid, giftid) VALUES ('" . $_SESSION['id'] . "' , '" . $giftid . "')";
+     $result_add_like = $conn->query($sql_add_like);
+        if($result_add_like) {
+          echo '1';
+        }
+        else
+        {
+          echo '2';
+        }
    }
-   else {
-     echo '2';
+   else
+   {
+     $sql_remove_like = "DELETE FROM favorited WHERE accountid = '" . $_SESSION['id'] . "' AND giftid = '" . $giftid . "'";
+     $result_remove_like = $conn->query($sql_remove_like);
+        if($result_remove_like) {
+          echo '3';
+        }
+        else
+        {
+          echo '4';
+        }
    }
  }
  else
