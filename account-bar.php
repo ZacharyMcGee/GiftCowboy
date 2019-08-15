a8583cd9c0a7f5523afc2b35c7fc3c4d91dd6730
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-15 06:26:04 -0500

    Create account-bar.php

---
+++ account-bar.php
@@ -0,0 +1,25 @@
+<div class="account-bar">
+  <div class="login-signup">
+    <?php
+    if (isset($_SESSION['loggedin'])) {
+      echo "<p>Welcome back <a href='account.php'>" . $_SESSION['name'] . "</a>!</p>";
+    }
+    else
+    {
+      echo "<p>Save your favorite gifts by <a href='login.php'>logging in</a> or <a href='signup.php'>signing up</a></p>";
+    }
+    ?>
+  </div>
+
+  <div class="account-info">
+    <?php
+    if (isset($_SESSION['loggedin'])) {
+    	echo "<a href='favorites.php'>My Favorites</a><p>|</p><a href='account.php'>My Account</a><p>|</p><a href='logout.php'>Logout</a>";
+    }
+    else
+    {
+      echo "<a href='login.php'>Login</a><p>|</p><a href='account.php'>Sign up</a>";
+    }
+    ?>
+  </div>
+</div>
