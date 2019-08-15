aa345a28224194a6e615a0d7d5df1ea094d64638
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-15 06:24:32 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<div class="header">
+  <div class="logo">
+    <a href="index.php"><img src="images/logo.png"></a>
+  </div>
+
+  <div class="menu">
+  <ul class="menu-ul">
+    <li><a class="active" href="#home">Home</a></li>
+    <li><a href="#news">Top Rated</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#about">About</a></li>
+  </ul>
+</div>
+</div>
