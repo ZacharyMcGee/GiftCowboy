968ed9a271e0f716fe71c750f15779cf6a18acc8
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-15 06:25:33 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,35 @@
+<div id="footer" class="footer">
+  <div class="footer-section">
+    <div class="vertical-menu">
+      <p class="active">About</p>
+      <a href="#">Source</a>
+      <a href="#">Information</a>
+      <a href="#">Site Owner</a>
+    </div>
+  </div>
+  <div class="footer-section">
+    <div class="vertical-menu">
+      <p class="active">Site</p>
+      <a href="#">Login</a>
+      <a href="#">Sign Up</a>
+      <a href="#">My Account</a>
+    </div>
+  </div>
+  <div class="footer-section">
+    <div class="vertical-menu">
+      <p class="active">Contact</p>
+      <a href="#">Email</a>
+      <a href="#">Suggest a Gift</a>
+      <a href="#">Report a Bug</a>
+    </div>
+  </div>
+  <div class="footer-section">
+    <div class="vertical-menu">
+      <p class="active">Social</p>
+      <a href="#">Instagram</a>
+      <a href="#">Facebook</a>
+      <a href="#">Twitter</a>
+    </div>
+  </div>
+</div>
+</div>
