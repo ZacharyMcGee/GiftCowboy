39c1d7390882737b54a801af9bb7542dd4145e0f
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-15 06:24:36 -0500

    Create headings.php

---
+++ headings.php
@@ -0,0 +1,13 @@
+<head>
+  <meta name="description" content="Webpage description goes here" />
+  <meta charset="utf-8">
+  <title>Gift Cowboy</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/toggle-switch.css">
+  <script src="https://kit.fontawesome.com/322089c541.js"></script>
+  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js"></script>
+  <script src="js/giftcowboy.js"></script>
+</head>
