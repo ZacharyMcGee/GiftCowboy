7a72ac98e0584396a04611f0a78738b32b7cf78c
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-16 06:28:29 -0500

    Update account.php

--- account.php
+++ account.php
@@ -1,6 +1,28 @@
 <?php
 session_start();
-if(!isset($_SESSION["username"])){
-header("Location: login.php");
-exit(); }
+// check to see if the user is logged in
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.php');
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "headings.php"; ?>
+
+<body>
+
+<?php include "account-bar.php"; ?>
+
+<?php include "header.php"; ?>
+
+
+<?php include "footer.php"; ?>
+
+<script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/css-toggle-switch@latest/dist/toggle-switch.css"/>
+</script>
+
+</body>
+</html>
