8e615e382beae75c744a48823dd86edfa599ce65
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-18 08:57:19 -0500

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   <div class="menu">
   <ul class="menu-ul">
     <li><a class="active" href="#home">Home</a></li>
-    <li><a href="#news">Top Rated</a></li>
+    <li><a href="top-rated.php">Top Rated</a></li>
     <li><a href="#contact">Contact</a></li>
     <li><a href="#about">About</a></li>
   </ul>
