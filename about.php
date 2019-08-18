0b25224d65855bc51707d3dc3f260deb21a262f5
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-18 15:09:20 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "headings.php"; ?>
+
+<body>
+
+<?php include "account-bar.php"; ?>
+
+<?php include "header.php"; ?>
+
+<div id="container" class="container">
+  <div id="top-rated-gifts" class="content">
+    <div class="content-title">
+      <p><img src="images/gift.png"> Top-Rated Gifts</p>
+    </div>
+    
+  </div>
+
+<?php include "footer.php"; ?>
+
+<script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/css-toggle-switch@latest/dist/toggle-switch.css"/>
+</script>
+
+</body>
+</html>
