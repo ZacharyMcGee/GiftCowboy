fdb7665d6316c95af6ed9a462576343eb127421e
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-18 17:30:50 -0500

    Update headings.php

--- headings.php
+++ headings.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/toggle-switch.css">
   <script src="https://kit.fontawesome.com/322089c541.js"></script>
-  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <script src="https://code.jquery.com/jquery-latest.min.js"></script>
   <script src="https://unpkg.com/infinite-scroll@3/dist/infinite-scroll.pkgd.min.js"></script>
   <script src="js/giftcowboy.js"></script>
 </head>
