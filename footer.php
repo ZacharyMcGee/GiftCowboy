dff34fac006dfcb0c3e4b01fba4e18ac21c5d3ca
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-29 06:39:00 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
   <div class="footer-section">
     <div class="vertical-menu">
       <p class="active">About</p>
-      <a href="#">Source</a>
+      <a href="https://github.com/ZacharyMcGee/GiftCowboy">Source</a>
       <a href="#">Information</a>
-      <a href="#">Site Owner</a>
+      <a href="https://www.zmcgee.com/">Site Owner</a>
     </div>
   </div>
   <div class="footer-section">
@@ -32,4 +32,7 @@
     </div>
   </div>
 </div>
+
+<div class="another-site">
+  <p>Another project by <a href="https://www.zmcgee.com/">Zachary McGee</a></p>
 </div>
