c7fc3da851eb2af43647831c3343bcea6e3c5dc5
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-29 06:39:15 -0500

    Create for-him.php

---
+++ for-him.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "headings.php"; ?>
+
+<body>
+
+<?php include "account-bar.php"; ?>
+
+<?php include "header.php"; ?>
+
+<div id="container" class="container">
+  <div id="for-him" class="content">
+    <div class="content-title">
+      <p><img src="images/gift.png"> For Him</p>
+    </div>
+
+  </div>
+
+<?php include "footer.php"; ?>
+
+<script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/css-toggle-switch@latest/dist/toggle-switch.css"/>
+</script>
+
+</body>
+</html>
