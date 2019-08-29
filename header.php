3107ca7dc7a71ee2aff1f47572c533df98a5e739
Zachary McGee <zachary.mcgee@siu.edu>
2019-08-29 06:39:39 -0500

    Update header.php

--- header.php
+++ header.php
@@ -6,8 +6,10 @@
   <div class="menu">
   <ul class="menu-ul">
     <li><a href="index.php">Home</a></li>
-    <li><a href="top-rated.php">Most Favorited</a></li>
-    <li><a href="least-rated.php">Least Favorited</a></li>
+    <li><a href="top-rated.php">Top Rated</a></li>
+    <li><a href="for-him.php">For Him</a></li>
+    <li><a href="for-her.php">For Her</a></li>
+    <li><a href="for-baby.php">For Baby</a></li>
   </ul>
 </div>
 </div>
